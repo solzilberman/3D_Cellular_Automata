9aee4c28c7cf6dac9d3e38db76a28b96cb798b93
solzilberman <sol.zilberman@gmail.com>
2021-11-21 17:35:18 -0500

    updated ffmpeg command

--- vid.sh
+++ vid.sh
@@ -2,5 +2,5 @@ rm ./images/*
 make
 ./main
 UUID=$(cat /proc/sys/kernel/random/uuid)
-ffmpeg -f image2 -i ./images/image%1d.png -r 90 -vf "setpts=1.5*PTS" test${UUID}.avi
-mv test${UUID}.avi /mnt/c/users/sol/Desktop/3dca_vids/test1${UUID}.avi
+ffmpeg -f image2 -i ./images/image%1d.png -r 30 -q 1 -crf 18 -vf "setpts=2*PTS" test${UUID}.avi
+mv test${UUID}.avi /mnt/c/users/sol/Desktop/3dca_vids/test${UUID}.avi
